a65b3b54a6babeab18dd96a9d98c131a4e2fabfe
Joel Niklaus <me@joelniklaus.ch>
2021-12-29 09:55:51 +0100

    adjusted script to filter German mc4 for legal data

--- filter_mc4.py
+++ filter_mc4.py
@@ -1,47 +1,84 @@
 import datetime
+import json
 import re
 from urllib.parse import urlparse
 
 import pandas as pd
 from datasets import load_dataset
 
+# TODO download more data at a time ==> streaming and decompressing is the bottleneck
 mc4 = load_dataset("mc4", languages=['de'], streaming=True)['train']
 
-base_terms = [r'Art\.\s*\d+', r'Abs\.\s*\d+', r'§']
+base_terms = [r'Art\.\s*\d+', r'Abs\.\s*\d+', r'§\s*\d+']
 latin_terms = ['in casu', 'de lege lata', 'de lege ferenda', ]
-additional_terms = ['Richter', 'Anwalt', 'Rechtssprechung', 'Verjährungsfrist', 'Verwirkungsfrist', 'Berufung',
-                    'Beschwerdeführer', 'Kläger', 'Beschwerdegegner']
+additional_terms = ['Richter', 'Anwalt', 'Gerichtshof', 'Rechtssprechung', 'Verjährungsfrist', 'Verwirkungsfrist',
+                    'Berufung', 'Beschwerdeführer', 'Kläger', 'Beschwerdegegner']
 terms = base_terms + latin_terms + additional_terms
-search_terms = '|'.join(terms)  # combine list into one regex for performance reasons
+search_terms = re.compile('|'.join(terms))  # combine list into one regex for performance reasons
+
+# TODO also create blacklist words
 
 print(f"Searching for {search_terms} in mc4")
 
+text_path = 'data/legal_mc4.csv'
+progress_path = 'data/progress.json'
 
-def status_update():
+
+def status_update(index):
     global begin_time
-    print(f"Found at least one of the search terms in {len(legal_mc4)} documents from {index + 1} searched.")
-    print(f"Blocked domains: {blocked_domains}")
+    print(
+        f"Found at least {min_num_matches} of the search terms "
+        f"in {len(legal_mc4['index'])} documents from {index + 1} searched.")
+    print(f"Blocked domains: {[domain for domain, value in domains.items() if value['blocked']]}")
     print(f"This iteration took {datetime.datetime.now() - begin_time}")
     print()
     begin_time = datetime.datetime.now()
 
 
-begin_time = datetime.datetime.now()
-blocked_domains = []  # add domains here which contain with high probability no legal text
+def save_and_reset(texts):
+    print(f"Saving {batch_size} texts to the file system")
+    df = pd.DataFrame(texts)
+    df.to_csv(text_path, mode='a', header=False)
+    return {"url": [], "text": [], "timestamp": [], "index": []}
+
+
+def save_and_report_progress(legal_mc4, current_index):
+    legal_mc4 = save_and_reset(legal_mc4)
+    status_update(current_index)
+    domains['current_index'] = current_index
+    with open(progress_path, 'w') as outfile:
+        json.dump(domains, outfile)
+    del domains['current_index']  # to cause no problems in status_update
+    return legal_mc4
+
+
 domains = {}  # contains one entry per domain
-confidence = 100
-threshold = 0.8
-legal_mc4 = list()
+confidence = 1000
+threshold = 0.99  # we don't want to miss out on texts
+min_num_matches = 5
+batch_size = 1e5
+
+legal_mc4 = {"url": [], "text": [], "timestamp": [], "index": []}
+df = pd.DataFrame(legal_mc4)
+df.to_csv(text_path)
+
+begin_time = datetime.datetime.now()
+
 for index, doc in enumerate(mc4):
     domain = urlparse(doc['url']).netloc
-    if domain in blocked_domains:  # as soon as a domain is in blocked_domains, skip the document
+    if domain in domains.keys() and domains[domain]["blocked"]:  # as soon as a domain is blocked, skip the document
         continue
 
     if domain not in domains:
-        domains[domain] = {"legal": 0, "non_legal": 0}  # add new empty entry for domain
+        # add new empty entry for domain
+        domains[domain] = {"legal": 0, "non_legal": 0, "blocked": False}
 
-    if re.search(search_terms, doc['text']):  # search terms in document
-        legal_mc4.append(doc)
+    matches = re.findall(search_terms, doc['text'])  # search terms in document
+    if len(matches) > min_num_matches:  # A manual sample search yielded only few false positives
+        legal_mc4["url"].append(doc["url"])
+        legal_mc4["text"].append(doc["text"])
+        legal_mc4["timestamp"].append(doc["timestamp"])
+        legal_mc4["index"].append(index)
         domains[domain]["legal"] += 1
     else:
         domains[domain]["non_legal"] += 1
@@ -50,17 +87,9 @@ for index, doc in enumerate(mc4):
     total = legal_num + non_legal_num
 
     if total >= confidence and non_legal_num / total > threshold:
-        blocked_domains.append(domain)
+        domains[domain]["blocked"] = True
 
-    if index % 1e5 == 0:
-        status_update()
+    if index % batch_size == 0:
+        legal_mc4 = save_and_report_progress(legal_mc4, index)
 
-status_update()
-
-df = pd.DataFrame(legal_mc4)
-df.to_json('data/legal_mc4.jsonl', orient='records', lines=True)
-
-# Analyze the links the documents came from
-# df = pd.read_json('data/legal_mc4.jsonl', lines=True)
-df.url = df.url.apply(lambda x: urlparse(x).netloc)
-print(df.url.unique())
+save_and_report_progress(legal_mc4, index)
---
+++ run_ubelix_job.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --job-name="Domains"
+#SBATCH --mail-user=joel.niklaus@inf.unibe.ch
+#SBATCH --mail-type=end,fail
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --time=15-00:00:00
+#SBATCH --mem=256GB
+#SBATCH --cpus-per-task=1
+#SBATCH --qos=job_epyc2_long
+#SBATCH --partition=epyc2
+
+# Put your code below this line
+
+python filter_mc4.py
+
+# IMPORTANT:
+# Run with                  sbatch run_ubelix_job.sh
+# check with                squeue --user=jn20t930 --jobs={job_id}
+# monitor with              scontrol show --detail jobid {job_id}
+# cancel with               scancel {job_id}
+# monitor gpu usage with    ssh gnode14 and then nvidia-smi
+# run interactive job with  srun --partition=gpu-invest --gres=gpu:rtx3090:1 --mem=64G --time=02:00:00 --pty /bin/bash
