e259e60903815fdc4d99eb9b3ec230ca0e51b505
Joel Niklaus <me@joelniklaus.ch>
2022-02-24 13:06:46 +0100

    adjusted filter script and added todos and adjusted domain retrieving script path

--- retrieve_domain_names.py
+++ retrieve_domain_names.py
@@ -4,7 +4,7 @@ import plotly.express as px
 import pandas as pd
 from datasets import load_dataset, tqdm
 
-link_path = 'data/links.csv'
+link_path = 'data/domains/links.csv'
 
 retrieve_domains = True
 
@@ -40,15 +40,15 @@ print(f"Reading domain names from {link_path}")
 df = pd.read_csv(link_path)
 print("Computing value counts of domains")
 domains = df.domain.value_counts().rename_axis('unique_domains').reset_index(name='counts')
-domains.to_csv('data/domains_all.csv')
+domains.to_csv('data/domains/domains_all.csv')
 
 top_ks = [10, 20, 50, 100, 200, 500, 1000, 2000, 5000]
 
 for top_k in top_ks:
     print(f"Processing top {top_k} value counts")
     top_k_df = domains.head(top_k)
-    top_k_df.to_csv(f'data/domains_top_{top_k:04d}.csv')
+    top_k_df.to_csv(f'data/domains/domains_top_{top_k:04d}.csv')
     fig = px.histogram(top_k_df, x="unique_domains", y="counts")
-    fig.write_image(f'data/domain-histogram_{top_k:04d}.png')
+    fig.write_image(f'data/domains/domain-histogram_{top_k:04d}.png')
 
 print("Finished running script")
