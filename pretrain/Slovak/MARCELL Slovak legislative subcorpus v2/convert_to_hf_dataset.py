cd76031f8dd0f462cdc0b930898eb40c25d63eaf
kapllan <veton-matoshi@hotmail.de>
2022-06-20 18:27:57 +0200

    Edited code to convert Slovakian MARCELL corpus to jsonl

--- pretrain/Slovak/MARCELL Slovak legislative subcorpus v2/convert_to_hf_dataset.py
+++ pretrain/Slovak/MARCELL Slovak legislative subcorpus v2/convert_to_hf_dataset.py
@@ -15,98 +15,141 @@ You can download the corpus here: https://elrc-share.eu/repository/browse/marcel
 
 
 def format_xml_file(path_to_file:str)->dict:
+
+    try:
     
-    content = open(path_to_file,'r').read()
-    soup = BeautifulSoup(content,'lxml')
-    
-    item = dict()
-    for x in soup.find_all('doc'):
+        content = open(path_to_file,'r').read()
+        soup = BeautifulSoup(content,'lxml')
         
-        try:
-            item['id']=x['id']
-        except:
-            item['id']=''
-        try:
-            item['date']=x['date']
-        except:
-            item['date']=''
-        try:
-            item['title']=x['title']
-        except:
-            item['title']=''
-        try:
-            item['url']=x['url']
-        except:
-            item['url']=''
-        item['language']='Slovak'
-        item['type']='legislation'
-        item['jurisdiction']='Slovakia'
-        item['metadata']=dict()
-        try:
-            item['metadata']['type']=x['type']
-        except:
-            item['metadata']['type']=''
-        try:
-            item['metadata']['entype']=x['entype']
-        except:
-            item['metadata']['entype']=''
-        try:
-            item['metadata']['keywords']=x['keywords']
-        except:
-            item['metadata']['keywords']=''
-        try:
-            item['metadata']['year']=x['year']
-        except:
-            item['metadata']['year']=''
-        try:
-            item['metadata']['position']=x['position']
-        except:
-            item['metadata']['position']=''
-        try:
-            item['metadata']['date_approved']=x['date_approved']
-        except:
-            item['metadata']['date_approved']=''
-        try:
-            item['metadata']['date_effect']=x['date_effect']
-        except:
-            item['metadata']['date_effect']=''
-        try:
-            item['metadata']['issuer']=x['issuer']
-        except:
-            item['metadata']['issuer']=''
-        try:
-            item['metadata']['eurovoc']=x['eurovoc']
-        except:
-            item['metadata']['eurovoc']=''
-        try:
-            item['metadata']['tld_score']=x['tld_score']
-        except:
-            item['metadata']['tld_score']=''
+        item = dict()
+        for x in soup.find_all('doc'):
+            
+            try:
+                item['id']=x['id']
+            except:
+                item['id']=''
+            try:
+                item['date']=x['date']
+            except:
+                item['date']=''
+            try:
+                item['title']=x['title']
+            except:
+                item['title']=''
+            try:
+                item['url']=x['url']
+            except:
+                item['url']=''
+            item['language']='Slovak'
+            item['type']='legislation'
+            item['jurisdiction']='Slovakia'
+            item['metadata']=dict()
+            try:
+                item['metadata']['type']=x['type']
+            except:
+                item['metadata']['type']=''
+            try:
+                item['metadata']['entype']=x['entype']
+            except:
+                item['metadata']['entype']=''
+            try:
+                item['metadata']['keywords']=x['keywords']
+            except:
+                item['metadata']['keywords']=''
+            try:
+                item['metadata']['year']=x['year']
+            except:
+                item['metadata']['year']=''
+            try:
+                item['metadata']['position']=x['position']
+            except:
+                item['metadata']['position']=''
+            try:
+                item['metadata']['date_approved']=x['date_approved']
+            except:
+                item['metadata']['date_approved']=''
+            try:
+                item['metadata']['date_effect']=x['date_effect']
+            except:
+                item['metadata']['date_effect']=''
+            try:
+                item['metadata']['issuer']=x['issuer']
+            except:
+                item['metadata']['issuer']=''
+            try:
+                item['metadata']['eurovoc']=x['eurovoc']
+            except:
+                item['metadata']['eurovoc']=''
+            try:
+                item['metadata']['tld_score']=x['tld_score']
+            except:
+                item['metadata']['tld_score']=''
+            try:
+                item['metadata']['file']=x['file']
+            except:
+                item['metadata']['file']=''
+            try:
+                item['metadata']['nadpis']=x['nadpis']
+            except:
+                item['metadata']['nadpis']=''
+            try:
+                item['metadata']['number']=x['number']
+            except:
+                item['metadata']['number']=''
+            try:
+                item['metadata']['podnadpis']=x['podnadpis']
+            except:
+                item['metadata']['podnadpis']=''
+            try:
+                item['metadata']['predpisdatum']=x['predpisdatum']
+            except:
+                item['metadata']['predpisdatum']=''
+            try:
+                item['metadata']['predpistyp']=x['predpistyp']
+            except:
+                item['metadata']['predpistyp']=''
+            try:
+                item['metadata']['tokcount']=x['tokcount']
+            except:
+                item['metadata']['tokcount']=''
+            try:
+                item['metadata']['tokcountdd']=x['tokcountdd']
+            except:
+                item['metadata']['tokcountdd']=''
+
+            
+            item['text']=''
+            
+
+        for s in soup.find_all('s'):
+            item['text']+=' '+s['text']
         
-        item['text']=''
-        
+        print(str(path_to_file).split('/')[-1])
+        return item
+    except:
+        print('Error')
+        print(path_to_file)
 
-    for s in soup.find_all('s'):
-        item['text']+=' '+s['text']
-    
-    
-    return item
 
-path= 'archive/' #Path to the downloaded folder
+if __name__=='__main__':
 
-path = Path(path)
+    path= './archive/' #Path to the downloaded folder
 
-files = [f for f in path.glob('**/*') if f.is_file()]
-files = [x for x in files if str(x).endswith('xml')][:100]
-print('Number of documents to be processed: ', len(files))
+    path = Path(path)
 
-df = pd.DataFrame(files,columns=['path'])
-df['item']=df.path.parallel_apply(lambda x: format_xml_file(x))
+    files = [f for f in path.glob('**/*') if f.is_file()]
+    files = [x for x in files if str(x).endswith('xml')]
+    print('Number of documents to be processed: ', len(files))
 
-items = df.item.tolist()
-df = pd.DataFrame(items)
+    '''
+    df = pd.DataFrame(files,columns=['path'])
+    df['item']=df.path.parallel_apply(lambda x: format_xml_file(x))
 
-#files_as_dict = [format_xml_file(x) for x in tqdm(files)]
-#df = pd.DataFrame(files_as_dict)
+    items = df.item.tolist()
+    df = pd.DataFrame(items)
+    '''
+    #
+    files_as_dict = [format_xml_file(x) for x in tqdm(files)]
+    df = pd.DataFrame(files_as_dict)
 
-df.to_json('Slovak_Slovakia_MARCELL_Slovak_legislative_subcorpus_v2.jsonl',force_ascii=False,orient='records', lines=True,indent=2)
+    df.to_json('Slovak_Slovakia_MARCELL_Slovak_legislative_subcorpus_v2.jsonl',force_ascii=False,orient='records', lines=True,indent=2)
