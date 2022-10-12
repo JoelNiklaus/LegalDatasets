cfd191f9a0fb6a714ee7d4754c8bb839d6dbe82f
Joel Niklaus <me@joelniklaus.ch>
2022-10-12 14:35:59 +0200

    updated czech corpus

--- pretrain/CzCDC/prepare_data.py
+++ pretrain/CzCDC/prepare_data.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import datasets
-from utils import save_and_compress
+from utils import save_and_compress, select_and_clean
 
 for court in ["ConCo", "SupAdmCo", "SupCo"]:
     df = pd.read_csv(f"{court}.csv", names=["file_name", "docket_number", "date", "court_abbreviation"],
@@ -9,5 +9,13 @@ for court in ["ConCo", "SupAdmCo", "SupCo"]:
     df["text"] = df.file_name.apply(lambda x: open(f"{court}/{x}", "r", encoding='latin-1').read().strip())
     print(df)
     df.drop(columns=["court_abbreviation"], inplace=True)
+
+    df['type'] = 'caselaw'
+    df['jurisdiction'] = 'Czechia'
+    df['language'] = 'cs'
+
+    df = select_and_clean(df)
+
     dataset = datasets.Dataset.from_pandas(df)
+
     save_and_compress(dataset, court, None)
