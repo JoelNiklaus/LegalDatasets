41a562ae1786f9bd3b3038f8c76f1c77a4924d4e
Joel Niklaus <me@joelniklaus.ch>
2022-10-12 11:02:08 +0200

    added code for Swiss legislation

---
+++ pretrain/swiss_legal_corpus/prepare_data.py
@@ -0,0 +1,31 @@
+import datasets
+import pandas as pd
+from bs4 import BeautifulSoup
+
+from utils import save_and_compress, select_and_clean
+pd.set_option('display.max_rows', 500)
+pd.set_option('display.max_columns', 500)
+pd.set_option('display.width', 1000)
+
+"""
+Case law and Legislation from Switzerland in de, fr, it 
+"""
+# somehow it doesn't work with datasets: https://github.com/huggingface/datasets/issues/3965
+#dataset = datasets.load_dataset("json", data_files=f"lexfind.jsonl")["train"]
+
+def parse_text(example):
+    if example['html_content']:
+        example['text'] = BeautifulSoup(example['html_content'], features="lxml").get_text()
+    else:
+        example['text'] = example['pdf_content']
+    return example
+
+df = pd.read_json('lexfind.jsonl', lines=True)
+df['type'] = 'legislation'
+df['jurisdiction'] = 'Switzerland'
+df = df.apply(parse_text, axis="columns")
+df = select_and_clean(df)
+
+for language in df.language.unique().tolist():
+    lang_df = df[df.language.str.contains(language)]
+    save_and_compress(lang_df, f'swiss_legislation_{language}')
