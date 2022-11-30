aabf9b66600944c9a944cc765c222019f8365f6b
kapllan <veton-matoshi@hotmail.de>
2022-11-30 12:23:16 +0100

    Added and edited some script to derive statistics

--- scripts/Statistics_Multi_Legal_Pile.py
+++ scripts/Statistics_Multi_Legal_Pile.py
@@ -12,48 +12,70 @@ import os
 tokenizer = AutoTokenizer.from_pretrained("xlm-roberta-base")
 
 
+def get_allready_processed_configs(output_file:str):
+    df = pd.read_csv(output_file)
+    configs = df.config.tolist()
+    return configs
+
+
 def get_overview(dataset_name, config):
-    results_scattered = list()
-
-    for split in ["train", "validation", "test"]:
-        try:
-            dataset = load_dataset(dataset_name, config, split=split, streaming=True)
-            print(dataset)
-            dataset = dataset.map(
-                lambda examples: tokenizer(examples["text"], add_special_tokens=False, return_length=True),
-                batched=True, batch_size=1000)
-            result_dict = dict()
-            Tokens = 0
-            Documents = 0
-            for document in dataset:
-                Tokens += document['length']
-                Documents += 1
-
-            result_dict['config'] = config
-            result_dict['Tokens'] = Tokens
-            result_dict['Documents'] = Documents
-            results_scattered.append(result_dict)
-        except Exception as e:
-            print(print_exc())
-            print('No ', split, ' in ', dataset_name, ' ', config)
-
-    results_scattered = pd.DataFrame(results_scattered)
-
-    final_result_dict = dict()
-    final_result_dict["config"] = config
-    final_result_dict["Tokens"] = results_scattered.Tokens.sum()
-    final_result_dict["Documents"] = results_scattered.Documents.sum()
-    final_result_dict["Tokens/Document"] = round(int(final_result_dict["Tokens"] / final_result_dict["Documents"]), 0)
 
     filename = re.sub(r'\/', '_', dataset_name)
     file_name = 'results_of_' + filename + '.csv'
 
     if os.path.isfile(file_name):
-        pd.DataFrame([final_result_dict]).to_csv(file_name, mode='a', header=False, index=False)
+        configs = get_allready_processed_configs(file_name)
     else:
-        pd.DataFrame([final_result_dict]).to_csv(file_name, index=False)
+        configs = []
 
-    return final_result_dict
+    if config not in configs:
+
+        results_scattered = list()
+
+        for split in ["train", "validation", "test"]:
+            try:
+                dataset = load_dataset(dataset_name, config, split=split, streaming=True)
+                print(dataset)
+                dataset = dataset.map(
+                    lambda examples: tokenizer(examples["text"], add_special_tokens=False, return_length=True),
+                    batched=True, batch_size=1000)
+                result_dict = dict()
+                Tokens = 0
+                Documents = 0
+                for document in dataset:
+                    Tokens += document['length']
+                    Documents += 1
+
+                result_dict['config'] = config
+                result_dict['Tokens'] = Tokens
+                result_dict['Documents'] = Documents
+                results_scattered.append(result_dict)
+            except Exception as e:
+                print(print_exc())
+                print('No ', split, ' in ', dataset_name, ' ', config)
+
+        results_scattered = pd.DataFrame(results_scattered)
+
+        final_result_dict = dict()
+        final_result_dict["config"] = config
+        final_result_dict["Tokens"] = results_scattered.Tokens.sum()
+        final_result_dict["Documents"] = results_scattered.Documents.sum()
+        try:
+            final_result_dict["Tokens/Document"] = round(int(final_result_dict["Tokens"] / final_result_dict["Documents"]), 0)
+        except:
+            final_result_dict["Tokens/Document"] = 0
+
+
+        if os.path.isfile(file_name):
+            pd.DataFrame([final_result_dict]).to_csv(file_name, mode='a', header=False, index=False)
+        else:
+            pd.DataFrame([final_result_dict]).to_csv(file_name, index=False)
+
+        return final_result_dict
+    
+    else:
+        print('Config ', config, ' already processed. We will skip it.')
+
 
 
 def create_overview(dataset_name, available_configs):
@@ -76,6 +98,8 @@ if __name__ == '__main__':
     # TODO also add individual sources of native multi_legal_pile data
     dataset_names = ['joelito/Multi_Legal_Pile', 'joelito/mc4_legal', 'joelito/eurlex_resources',
                      'pile-of-law/pile-of-law']
+
+    dataset_names = ['joelito/mc4_legal']
     iteration_dict = {dataset_name: get_dataset_config_names(dataset_name) for dataset_name in dataset_names}
 
     for dataset_name, configs in iteration_dict.items():
